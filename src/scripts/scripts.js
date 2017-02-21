aaba76b0929f7333c2b77e2dcb2cb30d338e607d
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-21 03:10:49 -0300

    remove

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,34 +1,10 @@
-$("document").ready(function(){
-
-  smoothScroll.init({
-    selector: '[data-scroll]',
-    speed: 1000,
-    easing: 'easeOutQuad',
-    offset: 110,
-  });
-
-  function animateTo(selector) {
-
-    var anchor = document.querySelector(selector);
-
-    smoothScroll.animateScroll(anchor, false, {
-      speed: 1000,
-      easing: 'easeOutQuad',
-      offset: 115,
-    });
-
-  }
+$("document").ready(function () {
 
   $('img[data-src]').each(function () {
-
     var img = $(this);
 
     if (img.data('src')) {
-
       img.attr('src', img.data('src'));
-
     }
-
   });
-
 });
